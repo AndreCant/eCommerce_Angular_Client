2082bd5c0b77e624124519f36231319343d90dc9
Andrea Cantagallo <andrea_cantagallo@libero.it>
2021-06-07 20:16:28 +0200

    Removed material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,35 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatTabsModule} from '@angular/material/tabs';
-import { NavbarComponent } from './commons/navbar/navbar.component';
-import {FlexLayoutModule} from "@angular/flex-layout";
-
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    FlexLayoutModule,
-    MatTabsModule,
-    MatButtonToggleModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
