36013dbbed5d01fae23cbcced1394240d4f6bdee
Andrea Cantagallo <andrea_cantagallo@libero.it>
2021-06-08 23:25:37 +0200

    Add navbar html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { NavbarComponent } from './commons/navbar/navbar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
