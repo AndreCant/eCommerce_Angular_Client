aef844747ebe2c075a2266d7171bcc55a63d0eea
Andrea Cantagallo <andrea_cantagallo@libero.it>
2021-06-25 18:34:22 +0200

    First step translation

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,7 @@
 export enum AppConstants {
     DOMAIN = '127.0.0.1:8000',
     SERVICES_BASE_URL = `http://127.0.0.1:8000/api/rest`, //Laravel port
-    LOGIN_STORAGE = 'login'
+    LOGIN_STORAGE = 'login',
+    IT_FLAG_PATH = "assets/flags/4x3/it.svg",
+    US_FLAG_PATH = "assets/flags/4x3/us.svg"
 }
--- src/app/components/commons/navbar/navbar.component.ts
+++ src/app/components/commons/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { AppConstants } from 'src/app/app.constants';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
@@ -9,10 +10,35 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
-
-  ngOnInit(): void {
+  currLanguage: any;
+  Object = Object;
+  languages: any = {
+    it: {
+      flag: AppConstants.IT_FLAG_PATH,
+      acronym: "ITA"
+    },
+    en: {
+      flag: AppConstants.US_FLAG_PATH,
+      acronym: "EN/US"
+    }
   }
+
+  constructor(private authService: AuthService, private translate: TranslateService) {
+    console.log(this.translate.currentLang);
+
+    this.setLanguage(this.translate.currentLang);
+  }
+
+  setLanguage(lang: string){
+    switch (lang) {
+      case "it": this.currLanguage = this.languages.it;
+      break;
+      default: this.currLanguage = this.languages.en;
+        break;
+    }
+  }
+
+  ngOnInit(): void {}
 
   logout(){
     this.authService.logout();
@@ -20,6 +46,11 @@ export class NavbarComponent implements OnInit {
 
   isAuthenticated(){
     return this.authService.isAuthenticated();
+  }
+
+  changeLanguage(lang: string){
+    this.translate.use(lang);
+    this.setLanguage(lang);
   }
 
 }
